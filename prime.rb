32b682384a2a70efe92ab5cdebcff24126e244b3
Angela Stephens <angelastephens317@gmail.com>
2020-06-06 03:47:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(n)
   if n < 0 or n == 0 or n == 1
     return false
   else
-  (2..n-1).to.a.all? do |possible_factor|
+    (2..n-1).to.a.all? do |possible_factor|
     n % possible_factor != 0
   end
 end
