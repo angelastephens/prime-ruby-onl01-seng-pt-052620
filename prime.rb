b87eda750423a5e191545b0d7cab47ca2826f7d3
Angela Stephens <angelastephens317@gmail.com>
2020-06-06 03:37:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,12 @@ def prime?(n)
   require 'prime'
 end
 
-def prime?(n)
-  n(1...10).prime?
+def prime(n)
+if n < 0 or n ==0 or n == 1
+return false
+else
+(2..n-1).to.a.all? do |possible_factor|
+n possible_factor !=
+end
+end
 end
